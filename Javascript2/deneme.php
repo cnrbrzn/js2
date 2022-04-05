63bfdd5856672d505f400d0767f38f415eea13ea
canerborazan <canerborazann@gmail.com>
2022-04-05 17:36:07 +0300

    05.04.2022

---
+++ Javascript2/deneme.php
@@ -0,0 +1,14 @@
+<?php
+
+
+
+$isim = $_POST["isim"];
+$dil = $_POST["dil"];
+
+
+
+
+//echo "PHP'den veri geldi...";
+echo $isim." isimli kişi ".$dil." dilini konuşuyor..";
+
+?>
---
+++ Javascript2/web.js
@@ -0,0 +1,69 @@
+$(function () {
+
+    $(".menu").on({
+        mouseover: function () {
+            $(this).css("color", "yellow");
+        },
+        mouseleave: function () {
+            $(this).css("color", "antiquewhite");
+        }
+    });
+
+    animasyon();
+
+    function animasyon(){
+
+        var say = 1;
+
+        setInterval(function(){
+            var a = "spor" + say + ".jpg";
+            $("#h_goruntu").fadeOut(3000,function(){
+                $(this).queue(function(){
+                    $(this).attr("src",a);
+                    $(this).dequeue();
+                });
+
+                $(this).fadeIn(3000);
+            });
+            say++;
+            if(say==6){
+                say=1;
+            }
+        },6000);
+
+
+    }
+
+    $("#kapama").on({
+        mouseover: function(){
+            $(this).css("cursor","pointer");
+        },
+        click: function(){
+            $("#cerez").css("display","none");
+        }
+    });
+
+    $(".kır").on({
+        mouseover:function(){
+            $(this).css("color", "red");
+        },
+        mouseleave: function () {
+            $(this).css("color", "gray");
+        }
+
+    });
+
+    $(".mavi").on({
+        mouseover:function(){
+            $(this).css("color", "blue");
+        },
+        mouseleave: function () {
+            $(this).css("color", "gray");
+        }
+
+    });
+
+
+
+
+});
